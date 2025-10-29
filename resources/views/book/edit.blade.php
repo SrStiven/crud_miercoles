6744e11fccefc98c92f8736087660c1c84b1ddb7
David Estiven Mendez Lara <estivenmendezlara2020@gmail.com>
2025-10-29 10:33:18 -0500

    Se incluyo Bootstrap

--- resources/views/book/edit.blade.php
+++ resources/views/book/edit.blade.php
@@ -1,46 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-     <h2>Crear libros</h2>
+@extends('layouts.app')
+@section('content')
+<div class="container p-3 my-3 border">
+    <div class="mx-auto" style="width:200px">
+        <h2>Crear libros</h2>
+    </div>
     <form action="{{ route('book.update') }}" method="POST">
         @csrf
         <input type="hidden" hidden="id" name="id" value="{{ $book->id }}">
-        <div>
+        <div class="form-group">
             <label> Nombre del autor</label>
             <input type="text" name="name" required value="{{ $book->name }}">
         </div>
         <br>
-        <div>
+        <div class="form-group">
             <label>Titulo del libro</label>
             <input type="text" name="title" required value="{{ $book->title }}">
         </div>
         <br>
-        <div>
+        <div class="form-group">
             <label>Cantidad de libros</label>
             <input type="number" name="count" required min="0" value="{{ $book->count }}">
         </div>
         <br>
-        <div>
+        <div class="form-group">
             <label>Fecha de vencimiento del libro</label>
             <input type="date" name="due_date" required>
         </div>
-        <div>
+        <div class="form-group">
             <label>Genero del libro</label>
-            <select name="gender">
+            <select name="gender" class="form-control">
                 <option value="">Seleccinar</option>
                 <option value="accion" {{ $book->gender == 'accion' ? 'selected' : '' }}>Accion</option>
                 <option value="comedia" {{ $book->gender == 'comedia' ? 'selected' : '' }}>Comedia</option>
                 <option value="ficcion" {{ $book->gender == 'ficcion' ? 'selected' : '' }}>Ficcion</option>
             </select>
         </div>
-        <a href="{{ route('book.index') }}">Regresar</a>
-        <button type="submit">Enviar</button>
+        <a href="{{ route('book.index') }}" class="btn btn-secondary">Regresar</a>
+        <button type="submit" class="btn btn-success">Enviar</button>
     </form>
-</body>
-</html>
+</div>
+  
+@endsection
+   
--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,37 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Crear libros</h2>
-    <form action="{{ route('book.create') }}" method="POST">
+@extends('layouts.app')
+@section('content')
+
+    <div class="container p-3 my-3 border">
+        <div class="mx-auto" style="width:200px">
+            <h2>Crear libros</h2>
+        </div>
+        
+        <form action="{{ route('book.create') }}" method="POST">
         @csrf
-        <div>
+        <div class="form-group">
             <label> Nombre del autor</label>
             <input type="text" name="name" required>
         </div>
-        <br>
-        <div>
+        <div class="form-group">
             <label>Titulo del libro</label>
             <input type="text" name="title" required>
         </div>
-        <br>
-        <div>
+        <div class="form-group">
             <label>Cantidad de libros</label>
             <input type="number" name="count" required min="0">
         </div>
-        <br>
-        <div>
+        <div class="form-group">
             <label>Fecha de vencimiento del libro</label>
             <input type="date" name="due_date" required>
         </div>
-        <div>
+        <div class="form-group">
             <label>Genero del libro</label>
-            <select name="gender">
+            <select name="gender" class="form-control">
                 <option value="">Seleccinar</option>
                 <option value="accion">Accion</option>
                 <option value="comedia">Comedia</option>
@@ -40,30 +35,41 @@
         </div>
         <button type="submit">Enviar</button>
     </form>
+    </div>
+ 
     <hr>
     <form action="{{ route('book.destroy') }}" method="POST" onsubmit="return confirm('Estas seguro')?">
         @csrf
         <div>
             <label>Eliminar todos los libros</label>
-            <button type="submit">Eliminar</button>
+            <button type="submit" class="btn btn-danger"> Eliminar</button>
         </div>
     </form>
     <hr>
-    <h2>Export / Import</h2>
-    <div>
-        <label>Export excel</label>
-        <a href="{{ route('book.export') }}">Exportar excel</a>
+    <div class="container">
+        <div class="container p-3 my-3 border">
+            <h2>Export</h2>
+            <label>Export excel</label>
+            <a href="{{ route('book.export') }}"  class="btn btn-light">Exportar excel</a>
     </div>
-    <form action="{{ route('book.import') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <div>
-        <label>Importar excel</label>
-        <input type="file" name="file">
-        <button type="submit">Cargar</button>
+
+    <div class="container p-3 my-3 border">
+        <h2>Import</h2>
+        <form action="{{ route('book.import') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div>
+            <label>Importar excel</label>
+            <input type="file" name="file">
+            <button type="submit" class="btn btn-secondary">Cargar</button>
+        </div>
+        </form>
     </div>
-    </form>
-    <hr>
-    <table border="1">
+
+</div>
+
+
+<div class="container">
+   <table class="table">
         <thead>
             <tr>
                 <th>Nombre</th>
@@ -90,5 +96,5 @@
             
         @endforeach
     </table>
-</body>
-</html>
+</div>
+@endsection
